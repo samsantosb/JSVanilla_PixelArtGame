3e2f5bba178dfb7624de231f420f6d356c2025f1
samsantosb <samsantosb@outlook.com>
2021-07-09 11:08:10 -0300

    promises

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const q = document.querySelector.bind(document);
+const q = document.querySelector.bind(document)
 const qll = document.querySelectorAll.bind(document);
 const select = () => {
-  q('#color-palette').addEventListener('click', (e) => {
+  q('#color-palette').addEventListener('click', async (e) => {
     q('.selected').classList.remove('selected');
-    e.target.classList.add('selected');
+    await e.target.classList.add('selected');
   });
 };
-const fill = () => q('#pixel-board').addEventListener('click', (e) =>
-  (e.target.style.backgroundColor = q('.selected').style.backgroundColor));
-const clear = () => q('#clear-board').addEventListener('click', () => {
-  const p = qll('.pixel');
+const fill = () => q('#pixel-board').addEventListener('click', async (e) =>
+  await (e.target.style.backgroundColor = q('.selected').style.backgroundColor));
+const clear = () => q('#clear-board').addEventListener('click', async () => {
+  const p = await qll('.pixel');
   for (let pxl of p) pxl.style.backgroundColor = 'white';
 });
 select(), fill(), clear();

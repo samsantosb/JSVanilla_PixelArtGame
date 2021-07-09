830af0c1c73303557577d5cf96f4409c6335b514
samsantosb <samsantosb@outlook.com>
2021-07-09 12:05:22 -0300

    aa

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const q = document.querySelector.bind(document)
+const q = document.querySelector.bind(document);
 const qll = document.querySelectorAll.bind(document);
-const select = () => {
-  q('#color-palette').addEventListener('click', (e) => {
-    q('.selected').classList.remove('selected');
-    e.target.classList.add('selected');
-  });
-};
+const select = () => q('#color-palette').addEventListener('click', (e) => {
+  q('.selected').classList.remove('selected');
+  e.target.classList.add('selected');
+});
 const fill = () => q('#pixel-board').addEventListener('click', (e) =>
-  (e.target.style.backgroundColor = q('.selected').style.backgroundColor));
+  e.target.style.backgroundColor = q('.selected').style.backgroundColor);
 const clear = () => q('#clear-board').addEventListener('click', () => {
-  for (let pxl of qll('.pixel')) pxl.style.backgroundColor = 'white';
+  for (const p of qll('.pixel')) p.style.backgroundColor = 'white';
 });
 select(), fill(), clear();
